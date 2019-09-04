bebfdfd0d639e050828e2bb3de4cd67ff6e93588
Kaan Ozdemir <kaan.ozdemir@tsoft.com.tr>
2019-09-04 23:49:54 +0300

    Initial Commit

---
+++ HTMLLinkDetectorExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HTMLLinkDetectorExample
+//
+//  Created by Kaan Ozdemir on 4.09.2019.
+//  Copyright © 2019 Kaan Ozdemir. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
